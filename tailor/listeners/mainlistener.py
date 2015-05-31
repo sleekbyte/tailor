77c114925fb45fa56c1da358ed47d8222775f99f
Aditya Trivedi <adityatriv@gmail.com>
2015-05-31 15:46:20 -0400

    Add overrides for UpperCamelCase construct names

--- tailor/listeners/mainlistener.py
+++ tailor/listeners/mainlistener.py
@@ -1,9 +1,19 @@
 from tailor.swift.swiftlistener import SwiftListener
 from tailor.utils.charformat import isUpperCamelCase
 
+
 class MainListener(SwiftListener):
 
     def enterClassName(self, ctx):
         className = ctx.getText()
         if not isUpperCamelCase(className):
             print('Line', str(ctx.start.line) + ':', 'Class names should be in UpperCamelCase')
+
+    def enterEnumName(self, ctx):
+        pass
+
+    def enterEnumCaseName(self, ctx):
+        pass
+
+    def exitStructName(self, ctx):
+        pass
