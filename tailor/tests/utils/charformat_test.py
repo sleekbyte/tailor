fbaa1b7045a4b9864de4dc087da2ad129580de71
Aditya Trivedi <adityatriv@gmail.com>
2015-05-31 17:57:40 -0400

    Add additional tests for is_upper_camel_case * UpperCamelCase name * blank name * lowerCamelCase name * snake_case name

--- tailor/tests/utils/charformat_test.py
+++ tailor/tests/utils/charformat_test.py
@@ -4,8 +4,17 @@ from tailor.utils import charformat
 
 class MyTestCase(unittest.TestCase):
 
+    def is_upper_camel_case_test_upper_camel_case_name(self):
+        self.assertTrue(charformat.is_upper_camel_case('HelloWorld'))
+
     def is_upper_camel_case_test_lower_camel_case_name(self):
         self.assertFalse(charformat.is_upper_camel_case('helloWorld'))
+
+    def is_upper_camel_case_test_blank_name(self):
+        self.assertFalse(charformat.is_upper_camel_case(''))
+
+    def is_upper_camel_case_test_snake_case_name(self):
+        self.assertFalse(charformat.is_upper_camel_case('Hello_World'))
 
 
 if __name__ == '__main__':
