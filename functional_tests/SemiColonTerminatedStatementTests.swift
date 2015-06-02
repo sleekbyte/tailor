c1fdcfd72b2156b4b63f1f3022854d0ff42622d2
Aditya Trivedi <adityatriv@gmail.com>
2015-06-02 14:09:15 -0400

    Add basic functional test case for semicolon rule

---
+++ functional_tests/SemiColonTerminatedStatementTests.swift
@@ -0,0 +1,17 @@
+import Foundation;
+
+println("Hello, World!")
+
+// Class examples
+
+class lowerCamelCase {
+    var x: String = "hello"
+
+    func demo()
+    {
+        for var x = 0; ; {
+            print(x);
+        };
+    }
+};
+
