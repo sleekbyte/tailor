a5a9807bb4c473442716201b6b220be62d764af4
Alykhan Kanji <github@alykhan.com>
2015-06-11 00:23:42 -0400

    Use Location namedtuple when calling printer

--- tailor/listeners/filelistener.py
+++ tailor/listeners/filelistener.py
@@ -1,3 +1,4 @@
+from tailor.types.location import Location
 from tailor.utils.sourcefile import num_lines_in_file, file_too_long
 
 
@@ -16,4 +17,4 @@ class FileListener:
             self.__printer.error('File is over maximum line limit (' +
                                  str(num_lines_in_file(self.__filepath)) +
                                  '/' + str(max_lines) + ')',
-                                 loc=(max_lines + 1, 0))
+                                 loc=Location(max_lines, 1))
