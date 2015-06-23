a1e23c8798a40cb0448420f30524058c3d42da19
Neha Singh <neha.singh2293@gmail.com>
2015-06-23 17:57:44 -0400

    Add functional tests for UpperCamelCase checks

---
+++ src/test/java/com/sleekbyte/tailor/functional/UpperCamelCaseTest.java
@@ -0,0 +1,19 @@
+package com.sleekbyte.tailor.functional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Functional tests for UpperCamelCase rule
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class UpperCamelCaseTest {
+
+    @Test
+    public void testUpperCamelCase() {
+        
+    }
+
+
+}
