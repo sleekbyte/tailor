f5bfe2ccc45f7e862cb9b167ca042d0df63a9d36
Aditya Trivedi <adityatriv@gmail.com>
2015-06-29 17:05:49 -0400

    Test ViolationMessage::compareTo()

--- src/test/java/com/sleekbyte/tailor/output/ViolationMessageTest.java
+++ src/test/java/com/sleekbyte/tailor/output/ViolationMessageTest.java
@@ -1,6 +1,7 @@
 package com.sleekbyte.tailor.output;
 
 import com.sleekbyte.tailor.common.Messages;
+import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,8 +10,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -24,11 +25,36 @@ public class ViolationMessageTest {
 
     @Before
     public void setUp() throws IOException {
-        ViolationMessage violationMessage = new ViolationMessage("/usr/bin/local", 10, 1, Messages.ERROR, "errMsg");
+        this.violationMessage = new ViolationMessage("/usr/bin/local", 10, 1, Messages.ERROR, "errMsg");
     }
 
     @Test
-    public void testCompareTo() throws Exception { }
+    public void testCompareTo() throws Exception {
+        // global message has lower line number than test message
+        ViolationMessage messageWithGreaterLineNumber = new ViolationMessage("/usr/bin/local", 12, 5, Messages.ERROR, "errMsg");
+        int ret = this.violationMessage.compareTo(messageWithGreaterLineNumber);
+        Assert.assertTrue(ret < 0);
+
+        // global message has greater line number than test message
+        ViolationMessage messageWithLesserLineNumber = new ViolationMessage("/usr/bin/local", 8, 5, Messages.ERROR, "errMsg");
+        ret = this.violationMessage.compareTo(messageWithLesserLineNumber);
+        Assert.assertTrue(ret > 0);
+
+        // global message has equal line number but greater column number than test message
+        ViolationMessage messageWithGreaterColumnNumber = new ViolationMessage("/usr/bin/local", 10, 2, Messages.ERROR, "errMsg");
+        ret = this.violationMessage.compareTo(messageWithGreaterColumnNumber);
+        Assert.assertTrue(ret < 0);
+
+        // global message has equal line number but lesser column number than test message
+        ViolationMessage messageWithLesserColumnNumber = new ViolationMessage("/usr/bin/local", 10, 0, Messages.WARNING, "warningMsg");
+        ret = this.violationMessage.compareTo(messageWithLesserColumnNumber);
+        Assert.assertTrue(ret > 0);
+
+        // global message has equal line number and column number wrt test message
+        ViolationMessage messageWithEqualLineColumnNumber = new ViolationMessage("/usr/bin/local", 10, 1, Messages.WARNING, "warningMsg");
+        ret = this.violationMessage.compareTo(messageWithEqualLineColumnNumber);
+        Assert.assertTrue(ret == 0);
+    }
 
     @Test
     public void testEquals() throws Exception {
@@ -38,7 +64,7 @@ public class ViolationMessageTest {
 
         // Unequal ViolationMessage
         ViolationMessage equalViolationMessage = new ViolationMessage("/usr/bin/local", 10, 1, Messages.ERROR, "errMsg");
-        assertTrue(violationMessage.equals(equalViolationMessage));
+        assertTrue(this.violationMessage.equals(equalViolationMessage));
     }
 
     @Test
