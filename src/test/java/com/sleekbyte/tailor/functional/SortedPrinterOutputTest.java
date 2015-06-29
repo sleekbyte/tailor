ef790de7338e3148d819984d4e2da1f551faa710
Aditya Trivedi <adityatriv@gmail.com>
2015-06-29 18:27:31 -0400

    Fix broken tests

--- src/test/java/com/sleekbyte/tailor/functional/SortedPrinterOutputTest.java
+++ src/test/java/com/sleekbyte/tailor/functional/SortedPrinterOutputTest.java
@@ -18,38 +18,36 @@ public class SortedPrinterOutputTest extends RuleTest {
 
     @Override
     protected void addAllExpectedMsgs() {
-        addExpectedMsg(1, 18, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(3, 25, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(6, 15, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(7, 15, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(9, 14, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(8, 14, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(9, 14, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(10, 2, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(13, 33, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(14, 29, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(15, 23, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(16, 39, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(17, 2, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(20, 8, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(21, 18, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(27, 10, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(31, 10, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(33, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(33, 2, Messages.ERROR, Messages.STATEMENTS);
-        addExpectedMsg(64, 2, Messages.ERROR, Messages.STATEMENTS);
+        addExpectedMsg(1, 18, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(3, 25, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(6, 15, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(7, 15, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(8, 14, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(9, 14, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(10, 2, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(13, 33, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(14, 29, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(15, 23, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(16, 39, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(17, 2, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(20, 8, Messages.ERROR, Messages.STRUCT + Messages.NAMES+ Messages.UPPER_CAMEL_CASE);
+        addExpectedMsg(21, 18, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(27, 10, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(31, 10, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
+        addExpectedMsg(33, Messages.ERROR, Messages.FILE + Messages.NEWLINE_TERMINATOR);
+        addExpectedMsg(33, 2, Messages.ERROR, Messages.STATEMENTS + Messages.SEMICOLON);
     }
 
     private void addExpectedMsg(int line, String classification, String msg) {
         expectedMessages.add(
                 Printer.genOutputStringForTest(
-                        inputFile.getName(), line, classification, msg + Messages.SEMICOLON));
+                        inputFile.getName(), line, classification, msg));
     }
 
     private void addExpectedMsg(int line, int column, String classification, String msg) {
         expectedMessages.add(
                 Printer.genOutputStringForTest(
-                        inputFile.getName(), line, column, classification, msg + Messages.SEMICOLON));
+                        inputFile.getName(), line, column, classification, msg));
     }
 
 }
