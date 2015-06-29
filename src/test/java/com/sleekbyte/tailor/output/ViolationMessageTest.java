f4cbc1fee7bb0dcec4b883b0c263e69012955534
Aditya Trivedi <adityatriv@gmail.com>
2015-06-29 15:05:44 -0400

    Add empty ViolationMessageTest class

---
+++ src/test/java/com/sleekbyte/tailor/output/ViolationMessageTest.java
@@ -0,0 +1,33 @@
+package com.sleekbyte.tailor.output;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import java.io.IOException;
+
+/**
+ * Tests for {@link ViolationMessage}
+ */
+@RunWith(MockitoJUnitRunner.class)
+
+public class ViolationMessageTest {
+
+    @Before
+    public void setUp() throws IOException {
+
+    }
+
+    @After
+    public void tearDown() { }
+
+    @Test
+    public void testCompareTo() throws Exception { }
+
+    @Test
+    public void testEquals() throws Exception { }
+
+    @Test
+    public void testToString() throws Exception { }
+}
