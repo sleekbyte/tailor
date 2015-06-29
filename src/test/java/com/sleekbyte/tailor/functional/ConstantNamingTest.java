420ef73ec849dc989cbf22673cd8d4f07988605a
Aditya Trivedi <adityatriv@gmail.com>
2015-06-29 17:43:34 -0400

    Sort expected output

--- src/test/java/com/sleekbyte/tailor/functional/ConstantNamingTest.java
+++ src/test/java/com/sleekbyte/tailor/functional/ConstantNamingTest.java
@@ -19,24 +19,23 @@ public class ConstantNamingTest extends RuleTest {
     @Override
     protected void addAllExpectedMsgs() {
         addExpectedMsg(2, 5, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
-        addExpectedMsg(11, 13, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
-        addExpectedMsg(12, 15, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
-        addExpectedMsg(20, 5, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
-        addExpectedMsg(24, 5, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
-        addExpectedMsg(56, 5, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
-        addExpectedMsg(67, 9, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
-        addExpectedMsg(78, 9, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
-
         addExpectedMsg(5, 16, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
         addExpectedMsg(5, 45, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
+        addExpectedMsg(11, 13, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
+        addExpectedMsg(12, 15, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
         addExpectedMsg(15, 9, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
-        addExpectedMsg(29, 33, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
+        addExpectedMsg(20, 5, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
+        addExpectedMsg(24, 5, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
         addExpectedMsg(29, 19, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
+        addExpectedMsg(29, 33, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
         addExpectedMsg(30, 7, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
         addExpectedMsg(31, 7, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
         addExpectedMsg(33, 7, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
         addExpectedMsg(40, 14, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
         addExpectedMsg(50, 11, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
+        addExpectedMsg(56, 5, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
+        addExpectedMsg(67, 9, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
+        addExpectedMsg(78, 9, Messages.ERROR, Messages.GLOBAL + Messages.CONSTANT + Messages.GLOBAL_CONSTANT_NAMING);
         addExpectedMsg(87, 17, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
         addExpectedMsg(94, 8, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
         addExpectedMsg(104, 14, Messages.ERROR, Messages.CONSTANT + Messages.CONSTANT_NAMING);
