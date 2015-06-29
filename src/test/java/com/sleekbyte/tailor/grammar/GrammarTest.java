c95464041210335c31af9cdb3ed1f1552a0a2ff2
Adithya <a4sriniv@uwaterloo.ca>
2015-06-29 12:57:44 -0400

    Add test to check if snippets parse correctly

---
+++ src/test/java/com/sleekbyte/tailor/grammar/GrammarTest.java
@@ -0,0 +1,46 @@
+package com.sleekbyte.tailor.grammar;
+
+import com.sleekbyte.tailor.Tailor;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class GrammarTest {
+
+    protected static final String TEST_INPUT_DIR = "src/test/java/com/sleekbyte/tailor/grammar/";
+    protected static final String NEWLINE_REGEX = "\\r?\\n";
+
+    protected ByteArrayOutputStream errContent;
+    protected ByteArrayOutputStream outContent;
+    protected File[] swiftFiles;
+
+    @Before
+    public void setUp() {
+        File curDir = new File(TEST_INPUT_DIR);
+        swiftFiles = curDir.listFiles((File file, String name) -> name.endsWith(".swift"));
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testRule() throws IOException {
+        for ( File swiftFile: swiftFiles) {
+            errContent = new ByteArrayOutputStream();
+            System.setErr(new PrintStream(errContent));
+            String[] command = { (TEST_INPUT_DIR + swiftFile.getName()) };
+            Tailor.main(command);
+            assertEquals(errContent.toString(), "");
+            System.setErr(null);
+        }
+    }
+
+}
