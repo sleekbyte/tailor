f5bc20df768fe6d80123f11eb9840775336dc51d
Aditya Trivedi <adityatriv@gmail.com>
2015-07-15 14:55:03 -0400

    Remove redundant whitespace

--- src/main/java/com/sleekbyte/tailor/listeners/MainListener.java
+++ src/main/java/com/sleekbyte/tailor/listeners/MainListener.java
@@ -162,7 +162,7 @@ public class MainListener extends SwiftBaseListener {
     @Override
     public void enterConstantDeclaration(SwiftParser.ConstantDeclarationContext ctx) {
         listenerHelper.evaluatePatternInitializerList(ctx.patternInitializerList(),
-             new ConstantDecListener(this.printer, this.maxLengths));
+            new ConstantDecListener(this.printer, this.maxLengths));
     }
 
     @Override
