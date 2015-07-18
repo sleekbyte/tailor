0302509dced78cf34af03721421a8d9f0ec6dc64
Aditya Trivedi <adityatriv@gmail.com>
2015-07-18 15:42:37 -0400

    Fix #119 Move enum name length check to another listener method

--- src/main/java/com/sleekbyte/tailor/listeners/MainListener.java
+++ src/main/java/com/sleekbyte/tailor/listeners/MainListener.java
@@ -43,11 +43,6 @@ public class MainListener extends SwiftBaseListener {
     }
 
     @Override
-    public void enterEnumCaseName(SwiftParser.EnumCaseNameContext ctx) {
-        verifier.verifyNameLength(Messages.ENUM_CASE + Messages.NAME, verifier.maxLengths.maxNameLength, ctx);
-    }
-
-    @Override
     public void enterStructName(SwiftParser.StructNameContext ctx) {
         verifier.verifyUpperCamelCase(Messages.STRUCT + Messages.NAMES, ctx);
         verifier.verifyNameLength(Messages.STRUCT + Messages.NAME, verifier.maxLengths.maxNameLength, ctx);
@@ -393,10 +388,14 @@ public class MainListener extends SwiftBaseListener {
     @Override
     public void enterRawValueStyleEnumCase(SwiftParser.RawValueStyleEnumCaseContext ctx) {
         verifier.verifyUpperCamelCase(Messages.ENUM_CASE+ Messages.NAMES, ctx.enumCaseName());
+        verifier.verifyNameLength(Messages.ENUM_CASE + Messages.NAME, verifier.maxLengths.maxNameLength,
+                                  ctx.enumCaseName());
     }
 
     @Override
     public void enterUnionStyleEnumCase(SwiftParser.UnionStyleEnumCaseContext ctx) {
         verifier.verifyUpperCamelCase(Messages.ENUM_CASE + Messages.NAMES, ctx.enumCaseName());
+        verifier.verifyNameLength(Messages.ENUM_CASE + Messages.NAME, verifier.maxLengths.maxNameLength,
+                                  ctx.enumCaseName());
     }
 }
