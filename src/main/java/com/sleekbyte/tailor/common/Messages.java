3c3fe004a741c844a49f019c84345832d7ba365b
Aditya Trivedi <adityatriv@gmail.com>
2015-08-19 12:41:55 +0530

    Rename integrate opt to Xcode

--- src/main/java/com/sleekbyte/tailor/common/Messages.java
+++ src/main/java/com/sleekbyte/tailor/common/Messages.java
@@ -108,6 +108,6 @@ public class Messages {
     public static final String MAX_NAME_LENGTH_DESC = MAXIMUM + IDENTIFIER_NAME + LENGTH + IN_CHARS_DESC;
     public static final String MAX_STRUCT_LENGTH_DESC = MAXIMUM + STRUCT + LENGTH + IN_CHARS_DESC;
     public static final String MAX_SEVERITY_DESC = MAXIMUM + SEVERITY + "[" + ERROR + "|" + WARNING + "]";
-    public static final String XCODE_INTEGRATION_DESC = "Integrate tailor into xcodeproj";
+    public static final String XCODE_INTEGRATION_DESC = "integrate Tailor into xcodeproj";
 
 }
