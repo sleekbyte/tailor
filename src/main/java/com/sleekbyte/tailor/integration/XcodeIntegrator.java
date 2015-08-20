3c7b21dd2458c7039d3f582aa5249a5d974b4b77
Aditya Trivedi <adityatriv@gmail.com>
2015-08-20 12:55:53 +0530

    Add javadoc

--- src/main/java/com/sleekbyte/tailor/integration/XcodeIntegrator.java
+++ src/main/java/com/sleekbyte/tailor/integration/XcodeIntegrator.java
@@ -1,11 +1,30 @@
 package com.sleekbyte.tailor.integration;
 
+import com.sleekbyte.tailor.common.ExitCode;
+
+import java.io.IOException;
+
 /**
  * Integrates Tailor into supplied Xcode project.
  */
 public final class XcodeIntegrator {
 
-    public static void setupXcode(String xcodeprojFilePath) {
-        System.out.println("PATH: " + xcodeprojFilePath);
+    private static final String xcodeIntegrationRubyScriptPath = "path to ruby script";
+
+    /**
+     * Integrate Tailor into suppled Xcode project as a build phase run script.
+     *
+     * @param relXcodeprojFilePath relative/absolute path to a xcodeproj file
+     */
+    public static void setupXcode(String relXcodeprojFilePath) {
+        String[] commands = {xcodeIntegrationRubyScriptPath, "absolute path to xcodeproj"};
+        try {
+            Process process = Runtime.getRuntime().exec(commands);
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Tailor - Xcode integration failed. Reason: " + e.getMessage());
+            System.exit(ExitCode.FAILURE);
+        }
+        System.exit(ExitCode.SUCCESS);
     }
 }
