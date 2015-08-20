12e2c0da17fcff88a25522f3dc66e74365774841
Aditya Trivedi <adityatriv@gmail.com>
2015-08-21 01:45:06 +0530

    Integrate Tailor into xcodeproj

--- src/main/java/com/sleekbyte/tailor/integration/XcodeIntegrator.java
+++ src/main/java/com/sleekbyte/tailor/integration/XcodeIntegrator.java
@@ -3,26 +3,28 @@ package com.sleekbyte.tailor.integration;
 import com.sleekbyte.tailor.common.ExitCode;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
 
 /**
  * Integrates Tailor into supplied Xcode project.
  */
 public final class XcodeIntegrator {
 
-    private static final String xcodeIntegrationRubyScriptPath = "";
-
     /**
      * Integrate Tailor into supplied Xcode project as a build phase run script.
      *
-     * @param relXcodeprojFilePath relative/absolute path to a xcodeproj file
+     * @param xcodeprojPath relative/absolute path to a xcodeproj file
      */
-    public static void setupXcode(String relXcodeprojFilePath) {
-
+    public void setupXcode(String xcodeprojPath) {
         try {
-            String absXcodeprojFilePath = getAbsolutePath(relXcodeprojFilePath);
-            System.out.println("ABSOLUTE PATH: " + absXcodeprojFilePath);
-        } catch (IOException e) {
+            integrateTailor(getAbsolutePath(xcodeprojPath));
+        } catch (IOException | InterruptedException e) {
             System.err.println("Tailor-Xcode Integration failed. Reason: " + e.getMessage());
             System.exit(ExitCode.FAILURE);
         }
@@ -30,13 +32,51 @@ public final class XcodeIntegrator {
         System.exit(ExitCode.SUCCESS);
     }
 
-    private static String getAbsolutePath(String relativePath) throws IOException {
-        File file = new File(relativePath);
+    private String getAbsolutePath(String path) throws IOException {
+        File file = new File(path);
         if (file.isDirectory()
-            && (relativePath.endsWith(".xcodeproj/") || relativePath.endsWith(".xcodeproj"))) {
+            && (path.endsWith(".xcodeproj/") || path.endsWith(".xcodeproj"))) {
             return file.getAbsolutePath();
         } else {
-            throw new IOException(relativePath + " is not a valid .xcodeproj");
+            throw new IOException(path + " is not a valid .xcodeproj");
         }
+    }
+
+    private void integrateTailor(String absolutePath) throws IOException, InterruptedException {
+        File tempScript = createTempRubyScript(absolutePath);
+
+        try {
+            ProcessBuilder pb = new ProcessBuilder("ruby", tempScript.toString());
+            pb.inheritIO();
+            Process process = pb.start();
+            process.waitFor();
+        } finally {
+            if (!tempScript.delete()) {
+                throw new FileNotFoundException("Failed to delete file " + tempScript );
+            }
+        }
+    }
+
+    private File createTempRubyScript(String absolutePath) throws IOException {
+        File tempScript = File.createTempFile("xcode_integrate", "rb");
+        Writer streamWriter = new OutputStreamWriter(new FileOutputStream(tempScript), Charset.forName("UTF-8"));
+        PrintWriter printWriter = new PrintWriter(streamWriter);
+
+        printWriter.println("#!/usr/bin/env ruby");
+        printWriter.println("require 'xcodeproj'");
+        printWriter.println("begin");
+        printWriter.println(String.format("\tproject = Xcodeproj::Project.open(\"%s\")", absolutePath));
+        printWriter.println("rescue");
+        printWriter.println("\tabort(\"Integration Error: Invalid .xcodeproj file\")");
+        printWriter.println("end");
+        printWriter.println("main_target = project.targets.first");
+        printWriter.println("phase = main_target.new_shell_script_build_phase(\"Tailor\")");
+        printWriter.println("phase.shell_script = \"/usr/local/bin/tailor\"");
+        printWriter.println("project.save()");
+
+        printWriter.close();
+        streamWriter.close();
+
+        return tempScript;
     }
 }
