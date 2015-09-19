4e2d889f5626405b64f17bb2f19fb5fe32c00a0b
Alykhan Kanji <github@alykhan.com>
2015-09-19 03:08:17 -0400

    Initial commit

---
+++ uninstall.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env sh
+
+set -e
+
+black='\033[0;30m'
+red='\033[1;31m'
+green='\033[1;32m'
+blue='\033[1;34m'
+reset='\033[0m'
+
+PREFIX="/usr/local"
+TAILOR_DIR="$PREFIX/tailor"
+BIN_DIR="$PREFIX/bin"
+START_SCRIPT="$TAILOR_DIR/bin/tailor"
+
+wait_for_user() {
+  if [ $(uname) = "Darwin" ]; then
+    read -n 1 -a CONTINUE -p "Press [y/N] to continue: " < /dev/tty
+  elif [ $(uname) = "Linux" ]; then
+    echo -n "Press [y/N] to continue: "
+    read CONTINUE < /dev/tty
+  fi
+  echo; echo
+  [ "$CONTINUE" = "y" ] || [ "$CONTINUE" = "Y" ]
+}
+
+maybe_sudo() {
+  if [ -w "$PREFIX" ]; then
+    "$@"
+  else
+    /usr/bin/sudo "$@"
+  fi
+}
+
+kill_sudo() {
+  if [ ! -w "$PREFIX" ]; then
+    /usr/bin/sudo -k
+  fi
+}
+
+cecho() {
+  message=$1
+  color=${2:-$black}
+  echo "$color$message$reset"
+}
+
+echo "Tailor will be uninstalled from: $blue$TAILOR_DIR/$reset"
+if wait_for_user; then
+  maybe_sudo /bin/rm -rf "$TAILOR_DIR"
+  maybe_sudo /bin/rm -f "$BIN_DIR"/tailor
+  kill_sudo
+
+  cecho "Tailor uninstalled." $green
+else
+  cecho "Tailor uninstallation cancelled." $red
+fi
