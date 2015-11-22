b17ccd3e9af5955f6265c790aaba87eae7626eb3
Adithya <a4sriniv@uwaterloo.ca>
2015-11-21 23:00:40 -0800

    Fix broken test

--- src/test/java/com/sleekbyte/tailor/functional/BraceWhitespaceTest.java
+++ src/test/java/com/sleekbyte/tailor/functional/BraceWhitespaceTest.java
@@ -93,6 +93,6 @@ public class BraceWhitespaceTest extends RuleTest {
 
     @Override
     protected String[] getCommandArgs() {
-        return new String[] { "--only=brace-whitespace" };
+        return new String[] { "--only=brace-style" };
     }
 }
