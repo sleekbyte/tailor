540b2bb71ee5cb2128f0d0ab2fee1e354925cbaa
Aditya Trivedi <adityatriv@gmail.com>
2015-12-07 10:18:19 +0530

    Add tests for parenthesis whitespace rule

---
+++ src/test/swift/com/sleekbyte/tailor/functional/ParenthesisWhitespaceTest.swift
@@ -0,0 +1,54 @@
+print("Hello, World!")
+
+if ( true ) {
+
+}
+
+if ( true) {
+
+}
+
+if (true) {
+
+}
+
+func demo( ) {
+
+}
+
+func eemo() {}
+
+func aemo( ) {}
+
+func gemo( c: Int ) {}
+
+func sdemo( c: Int) {}
+
+func nemo(c: Int ) {}
+
+func sum() -> ( ) {}
+
+let g = (5)
+
+let h = (5 )
+
+let tuple = ( 5, 2)
+
+func simpleMax<T: Comparable>(x: T, _ y: T) -> T {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+func simpleMaxWithWhitespace<T: Comparable>( x: T, _ y: T) -> T {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+func someMethod(a: String,
+    b: Int ) {
+
+}
