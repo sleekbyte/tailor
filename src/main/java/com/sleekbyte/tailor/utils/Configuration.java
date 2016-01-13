3d7299cb04e7934c1fe65d445191820e41d19883
Aditya Trivedi <adityatriv@gmail.com>
2016-01-13 17:57:01 -0500

    Add stub for Configuration

---
+++ src/main/java/com/sleekbyte/tailor/utils/Configuration.java
@@ -0,0 +1,9 @@
+package com.sleekbyte.tailor.utils;
+
+import com.sleekbyte.tailor.interfaces.ConfigurationInterface;
+
+/**
+ * Adaptor class for YamlConfiguration and CliArgumentParser.
+ */
+public final class Configuration implements ConfigurationInterface {
+}
