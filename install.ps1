8f185068c4760eacb3abc485c0a0a162d00030fc
Alykhan Kanji <github@alykhan.com>
2016-04-23 16:00:19 -0400

    Update install scripts to v0.9.0

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@ $erroractionpreference = 'stop' # Quit if anything goes wrong
 
 ### Variables
 $appdatadir = $env:localappdata.tolower()
-$tailorversion = '0.8.1'
+$tailorversion = '0.9.0'
 $tailor = 'tailor'
 $tailorzip = "$tailor-$tailorversion.zip"
 $tailorinstalldir = "$appdatadir\$tailor"
