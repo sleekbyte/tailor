29c731ac8d7d0197ee16cd2b774dfa6759b1e801
Aditya Trivedi <adityatriv@gmail.com>
2017-03-26 20:05:26 -0700

    Bump version number to v0.12.0

--- script/uninstall.ps1
+++ script/uninstall.ps1
@@ -3,7 +3,7 @@
 $erroractionpreference = 'stop' # Quit if anything goes wrong
 
 ### Variables
-$tailorversion = '0.11.1'
+$tailorversion = '0.12.0'
 $tailor = 'tailor'
 $appdatadir = $env:localappdata.tolower()
 $tailordir = "$appdatadir\$tailor"
